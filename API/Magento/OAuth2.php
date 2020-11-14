10f3ecc58fe630646a57de55222e7fc83627621b
Slava <you@example.com>
2020-11-14 19:17:37 +0200

    add OAuth2.php magento

---
+++ API/Magento/OAuth2.php
@@ -0,0 +1,31 @@
+<?php 
+
+class MagentoApiFacade
+{
+    private function _getToken(string $credentials)
+    {
+        $curl = curl_init(
+            'https:///rest/all/V1/integration/admin/token'
+        );
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Accept: application/json',
+            'Content-Type: application/json',
+        ));
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $credentials);
+
+        $result = curl_exec($curl);
+
+        if (curl_errno($curl)) {
+            throw new Exception(curl_error($curl));
+        }
+
+        curl_close($curl);
+
+        return json_decode($result);
+    }
+}
+
+$credentials = "{\"username\": \"{$username}\", \"password\": \"{$password}\"}";
