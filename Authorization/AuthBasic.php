5ef6de9187ff308620a08367105a29f75287f551
Stryker <>
2023-02-23 14:48:56 +0200

    refactoring by codestyle

--- Authorization/AuthBasic.php
+++ Authorization/AuthBasic.php
@@ -1,8 +1,8 @@
 <?php
-if (!isset($_SERVER['PHP_AUTH_USER'])){
-	header('WWW-Authenticate: Basic realm="My protected administration"');
-	header('HTTP/1.0 401 Unathorized');
-	echo 'You not enter your data';
-    } else	{
-		echo $_SERVER['PHP_AUTH_USER'];
-	  }
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My protected administration"');
+    header('HTTP/1.0 401 Unathorized');
+    echo 'You not enter your data';
+} else {
+    echo $_SERVER['PHP_AUTH_USER'];
+}
